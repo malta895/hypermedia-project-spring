48f78ade1a904bf3f225bba01837571314729f45
Luca Maltagliati <malta95@hotmail.it>
2021-09-12 17:38:29 +0200

    chore: initial commit of Java Spring Boot Initializr project

---
+++ src/main/java/com/malta895/hypermediaproject/HypermediaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.malta895.hypermediaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypermediaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HypermediaProjectApplication.class, args);
+    }
+
+}
